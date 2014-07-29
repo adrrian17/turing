e08a8a9d9d37d00b2aa38deb85eac36418b10bb5
Adrian Ayala <adrian.ayala17@gmail.com>
2014-07-28 23:45:22 -0500

    Adds Bundler and Jekyll to the bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+echo "Provisioning virtual machine..."
+
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get -y upgrade
 
-sudo apt-get install -y curl git libcurl4-openssl-dev freetds-dev nodejs
+sudo apt-get install -y curl git libcurl4-openssl-dev freetds-dev nodejs bundler
 
-curl -L https://get.rvm.io | bash -s stable
+sudo curl -L https://get.rvm.io | bash -s stable
+
+sudo gem install jekyll
 
 sudo npm install -g coffee-script
 sudo npm install -g gulp
+
+echo "Finished provisioning."
