824c3c71a558a1931d91c867e86f5bd03cefa1f5
Adrian Ayala <adrian.ayala17@gmail.com>
2014-08-02 12:41:20 -0500

    Uncomments the bootstrap.sh lines

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,16 +2,14 @@
 echo "Provisioning virtual machine..."
 
 # Upgrading system
-# sudo apt-get update
-# sudo apt-get -y upgrade
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get -y upgrade
 
 # Dependencies
-#sudo add-apt-repository -y ppa:chris-lea/node.js
-#####sudo apt-get install -y curl git libcurl4-openssl-dev freetds-dev #nodejs
+sudo apt-get install -y curl git libcurl4-openssl-dev freetds-dev nodejs
 
 # Npm packages
-#sudo apt-get install -y npm
-#sudo npm install -g coffee-script
-#sudo npm install -g gulp
-
-echo "Finished provisioning."
+sudo apt-get install -y npm
+sudo npm install -g coffee-script
+sudo npm install -g gulp
